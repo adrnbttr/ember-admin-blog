fddc2781aa165c2ed01ed951c9716e0d5fee046e
Adrien Bouttier <hibigbob@gmail.com>
2015-10-19 23:00:36 +0200

    Add index route

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   beforeModel(transition) {
-    if (!localStorage.getItem('isAuthenticate')) {
+    if (!this.controllerFor('login').get('authenticate').get('userIsLoggedIn')) {
+      var loginController = this.controllerFor('login');
+      loginController.set('previousTransition', transition);
       this.transitionTo('login');
     }
   },
