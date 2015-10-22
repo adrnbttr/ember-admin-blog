44e531d456a8f9733b05e9d61a6722c0d1b298a9
Adrien Bouttier <hibigbob@gmail.com>
2015-10-22 22:41:47 +0200

    Add adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,12 @@
 import DS from 'ember-data';
+import config from '../config/environment';
 
 export default DS.JSONAPIAdapter.extend({
-  host: 'http://localhost:4000'
+  authenticate: Ember.inject.service('authentication'),
+  host: config.apiUrl,
+  headers: Ember.computed('authenticate.accessToken', function() {
+    return {
+      "x-access-token": this.get('authenticate.accessToken')
+    };
+  })
 });
