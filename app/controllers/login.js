e83e7ec3860e3e5c1ae87c8e10a70289a5eba457
Adrien Bouttier <hibigbob@gmail.com>
2015-10-22 22:41:59 +0200

    Add login controller

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  authenticate: Ember.inject.service('authentication'),
+
+  actions: {
+    authenticate() {
+        var _this = this;
+        var previousTransition = _this.get('previousTransition');
+        var credentials = this.getProperties('identification', 'password');
+        this.get('authenticate').authenticate(credentials).then(function(){
+          if (previousTransition) {
+            _this.set('previousTransition', null);
+            previousTransition.retry();
+          } else {
+            // Default back to homepage
+            _this.transitionToRoute('index');
+          }
+        }).catch((message) => {
+            this.set('errorMessage', message);
+        });
+    }
+  }
 });
