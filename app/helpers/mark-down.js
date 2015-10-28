cd428083f99f289a39c2b4a36db24aa0cba7b4b7
Adrien Bouttier <hibigbob@gmail.com>
2015-10-28 22:15:36 +0100

    Add marked and browserify

---
+++ app/helpers/mark-down.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+import marked from 'npm:marked';
+
+export function markDown(params) {
+  return Ember.String.htmlSafe(marked(params[0]));
+}
+
+export default Ember.Helper.helper(markDown);
