e05ce2f6ade6584788e546ad8de74a2fb3c9019c
Adrien Bouttier <hibigbob@gmail.com>
2015-10-30 08:44:59 +0100

    Change from Json api adapter to rest adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 import config from '../config/environment';
 
-export default DS.JSONAPIAdapter.extend({
+export default DS.RESTAdapter.extend({
   authenticate: Ember.inject.service('authentication'),
   host: config.apiUrl,
   headers: Ember.computed('authenticate.accessToken', function() {
