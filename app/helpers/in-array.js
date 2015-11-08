487eb83efafc0a2c90033f92c8ee6c4b5c35dae5
Adrien Bouttier <hibigbob@gmail.com>
2015-11-08 21:07:29 +0100

    Change param order

--- app/helpers/in-array.js
+++ app/helpers/in-array.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export function inArray(params) {
-  return params[0].contains(params[1]); 
+  return params[1].contains(params[0]); 
 }
 
 export default Ember.Helper.helper(inArray);
