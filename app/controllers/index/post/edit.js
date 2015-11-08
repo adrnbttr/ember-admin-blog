74dae96433d5317c5ed2de32c2b4dc75c5b47e5c
Adrien Bouttier <hibigbob@gmail.com>
2015-11-08 21:07:53 +0100

    Change display of tags on edit page

--- app/controllers/index/post/edit.js
+++ app/controllers/index/post/edit.js
@@ -1,14 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+	tags: Ember.computed('model', function(){
+		let tags = this.get('model.tags');
+		let postTags = this.get('model.post.tags');
+
+		tags = tags.filter(function(tag){
+	       return postTags.contains(tag);
+	    });
+
+		return tags;
+	}),
+
 	actions: {
 		save(post) {
 			post.save();
 			this.transitionToRoute('index.posts');
 		},
 
-		close(tag) {
-			let post = this.get('model');
+		deleteTag(tag) {
+			let post = this.get('model.post');
 			post.get('tags').removeObject(tag);
 			post.save();
 		}
--- app/routes/index/post/edit.js
+++ app/routes/index/post/edit.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-  	// return this.store.peekRecord('post', params.post_id);
     return Ember.RSVP.hash({
 		post: this.store.findRecord('post', params.post_id),
 		tags: this.store.findAll('tag')
